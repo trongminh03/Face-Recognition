0184b13769e4cc4b3b0b70e11ace8d29ec0e7e81
Lê Trọng Minh <91895429+trongminh03@users.noreply.github.com>
2023-04-23 22:04:41 +0700

    .gitignore

---
+++ src/main1.py
@@ -0,0 +1,103 @@
+import cv2
+import os
+from time import time
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_lfw_people
+
+dataset_path = 'dataset/val' 
+WIDTH =  100
+HEIGHT = 100
+image_size = (WIDTH, HEIGHT) 
+
+images = [] 
+labels = []
+
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml') 
+
+# Load dataset to arrays
+for person_name in os.listdir(dataset_path): 
+    person_path = os.path.join(dataset_path, person_name) 
+    for img_name in os.listdir(person_path): 
+        img_path = os.path.join(person_path, img_name)
+        img = cv2.imread(img_path)  
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect face
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            face = gray[y:y+h, x:x+w]
+            face_resized = cv2.resize(face, image_size)
+            images.append(face_resized.flatten())  
+            # images.append(face_resized)
+            labels.append(person_name)
+
+X = np.array(images)   
+y = np.array(labels) 
+print(X.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+n_components = 150
+pca = PCA(n_components=n_components, svd_solver='randomized', whiten=True)
+pca.fit(X_train) 
+# pca.fit_transform(X_train)
+
+# eigenfaces = pca.components_.reshape((n_components, 64, 64))
+# print("Projecting the input data on the eigenfaces orthonormal basis")
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], } 
+print("Starting training, please wait ...")
+
+clf = GridSearchCV(
+    SVC(kernel ='rbf', class_weight ='balanced'), param_grid
+) 
+clf = clf.fit(X_train_pca, y_train) 
+
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
+
+print("Predicting people's names on the test set")
+
+y_pred = clf.predict(X_test_pca)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy) 
+# print(classification_report(y_test, y_pred, target_names = target_names))
+
+# Use the classifier to recognize faces in an input image
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml')
+# IMG_SIZE = 1850 
+WIDTH = 15
+HEIGHT = 10
+# cap = cv2.VideoCapture(0) 
+img = cv2.imread('test.jpg')
+while True: 
+    # ret, frame = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        face = gray[y:y+h, x:x+w]
+        face_resized = cv2.resize(face, (WIDTH, HEIGHT))
+        face_flat = face_resized.flatten()
+        # face_flat_pca = pca.transform(face_flat)
+        face_pred = clf.predict([face_flat])[0]
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(img, str(face_pred), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    cv2.imshow('Face Recognition', img)
+    # Exit program when 'q' key is pressed
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+  
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- src/test4.py
+++ src/test4.py
@@ -1,103 +1,56 @@
-import cv2
-import os
-from time import time
-import numpy as np
-from sklearn.model_selection import train_test_split
+import numpy as np 
+import cv2 
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from sklearn.decomposition import PCA
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import fetch_lfw_people
-
-dataset_path = 'dataset/val' 
-WIDTH =  100
-HEIGHT = 100
-image_size = (WIDTH, HEIGHT) 
-
-images = [] 
-labels = []
 
 face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml') 
+IMG_SIZE = 100
 
-# Load dataset to arrays
-for person_name in os.listdir(dataset_path): 
-    person_path = os.path.join(dataset_path, person_name) 
-    for img_name in os.listdir(person_path): 
-        img_path = os.path.join(person_path, img_name)
-        img = cv2.imread(img_path)  
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # Detect face
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            face = gray[y:y+h, x:x+w]
-            face_resized = cv2.resize(face, image_size)
-            images.append(face_resized.flatten())  
-            # images.append(face_resized)
-            labels.append(person_name)
+# Load training data
+X_train = np.load('X_train.npy')
+y_train = np.load('y_train.npy') 
 
-X = np.array(images)   
-y = np.array(labels) 
-print(X.shape)
+# Train SVM classifier
+svm = SVC(kernel='linear', probability=True)
+svm.fit(X_train, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+cap = cv2.VideoCapture(0) 
 
-n_components = 150
-pca = PCA(n_components=n_components, svd_solver='randomized', whiten=True)
-pca.fit(X_train) 
-# pca.fit_transform(X_train)
-
-# eigenfaces = pca.components_.reshape((n_components, 64, 64))
-# print("Projecting the input data on the eigenfaces orthonormal basis")
-X_train_pca = pca.transform(X_train)
-X_test_pca = pca.transform(X_test)
-
-param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], } 
-print("Starting training, please wait ...")
-
-clf = GridSearchCV(
-    SVC(kernel ='rbf', class_weight ='balanced'), param_grid
-) 
-clf = clf.fit(X_train_pca, y_train) 
-
-print("Best estimator found by grid search:")
-print(clf.best_estimator_)
-
-print("Predicting people's names on the test set")
-
-y_pred = clf.predict(X_test_pca)
-
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy) 
-# print(classification_report(y_test, y_pred, target_names = target_names))
-
-# Use the classifier to recognize faces in an input image
-face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml')
-# IMG_SIZE = 1850 
-WIDTH = 15
-HEIGHT = 10
-# cap = cv2.VideoCapture(0) 
-img = cv2.imread('test.jpg')
 while True: 
-    # ret, frame = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+    # Capture frame-by-frame 
+    ret, frame = cap.read()
+    
+    # Convert frame to grayscale 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
+    
+    # Detect faces in grayscale image
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5) 
+
+    # For each detected face, do face recognition
     for (x, y, w, h) in faces:
-        face = gray[y:y+h, x:x+w]
-        face_resized = cv2.resize(face, (WIDTH, HEIGHT))
-        face_flat = face_resized.flatten()
-        # face_flat_pca = pca.transform(face_flat)
-        face_pred = clf.predict([face_flat])[0]
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(img, str(face_pred), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    cv2.imshow('Face Recognition', img)
+        # Crop face region from grayscale image
+        face_gray = gray[y:y+h, x:x+w]
+        
+        # Resize face region to fixed size
+        face_gray_resized = cv2.resize(face_gray, (IMG_SIZE, IMG_SIZE))
+        
+        # Flatten resized face region to 1D array
+        face_flat = face_gray_resized.flatten()
+        
+        # Predict label for face region using SVM classifier
+        label = svm.predict([face_flat])[0]
+        
+        # Draw bounding box around face and label it with predicted name
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, str(label), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    
+    # Display the resulting frame
+    cv2.imshow('Face Recognition', frame)
+    
     # Exit program when 'q' key is pressed
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-  
-cv2.waitKey(0)
+
+# Release video capture object and close windows
+cap.release()
 cv2.destroyAllWindows()
